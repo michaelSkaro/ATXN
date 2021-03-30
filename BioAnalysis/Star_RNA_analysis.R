bd250f50cea8221e166a4fe1f3f50a0f431c588a
Michael Skaro <mfs61958@uga.edu>
2021-03-30 10:09:28 -0400

    functionalize the data preprocessing. Make mRNA counts

--- BioAnalysis/Star_RNA_analysis.R
+++ BioAnalysis/Star_RNA_analysis.R
@@ -14,29 +14,51 @@ library(stringr)
 setwd("/mnt/storage/mskaro1/ATXN/mRNA/concatenated/geneCounts")
 read_counts <- list.files()
 
-# make the counts input matrix
-for(i in read_counts){
-      if(file.exists(i)){
-        print(i)
-        dat <- data.table::fread(i, header = TRUE)
-        #print(i)
-        colnames(dat) <- c("Ensembl","unstranded_count","S1","S2")
-
-        pattern <- "[:alnum:]{2,3}-[:alnum:]{2,3}"
-        pattern2 <- '[^_]+'
-        match <- i %>% str_extract(pattern2)
-        #print(match)
+makeGeneCounts <- function(read_counts) {
+  cnt = 0
+  for(i in read_counts){
+    print(i)
+    dat <- data.table::fread(i, header = TRUE)
+    #print(i)
+    colnames(dat) <- c("Ensembl","unstranded_count","S1","S2")
+    print(colnames(dat))
+    pattern <- "[:alnum:]{2,3}-[:alnum:]{2,3}"
+    pattern2 <- '[^_]+'
+    match <- i %>% str_extract(pattern2)
+    
+    if(cnt == 0){
+      temp <- dat %>%
+        select(c(Ensembl,S2))
       
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        }
+      colnames(temp) <- c("Ensembl", match)
+      cnt = cnt +1
+    }else{
+      dat <- dat %>% dplyr::select(c(Ensembl,S2))
+      colnames(dat) <- c("Ensembl", match)
+      temp <- left_join(temp,dat, by = "Ensembl")
+      cnt = cnt +1
+    }
+    
+    print(str_glue("Processing: {i}"))
+    
+  }
+  df <- temp[4:length(temp$Ensembl),]
 }
-  
+
+
+df <- makeGeneCounts(read_counts)
+write.csv(df, "ATXN_mouse_RNAseq_counts.csv")
+###############################################
+setwd("/mnt/storage/mskaro1/ATXN/sra_downloads/mRNA_downloads/geneCounts")
+read_counts <- list.files()
+df2 <- makeGeneCounts(read_counts)
+colnames(df2) <- c("Ensembl","SRR1552444","SRR1552445","SRR1552446",
+                   "SRR1552447","SRR1552448","SRR1552449","SRR1552450",
+                   "SRR1552451","SRR1552452","SRR1552453","SRR1552454","SRR1552455")
+###############################################
+
+mammary_counts <- left_join(df,df2, by="Ensembl")
+
+write.csv(mammary_counts,"Mammary_mRNA_gene_counts.csv")
+
+
