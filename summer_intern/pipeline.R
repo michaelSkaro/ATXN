c7cd6c45324e2a3b265d1eb9bcb76fb1fdb764ec
Michael Skaro <mfs61958@uga.edu>
2021-06-29 11:43:03 -0400

    Update pipeline.R

--- summer_intern/pipeline.R
+++ summer_intern/pipeline.R
@@ -10,12 +10,18 @@ install.packages("corrplot")
 library(corrplot)
 
 
+# You'll want to change this here. You should code it to accept an argument at the command line as your filename you wll be using.
+# Or you can take a step further and direcly pass the directory.  
+
 filename <- "irisdataset.csv"
 dir <- as.character(filename)
 samples <- read.csv(dir)
 samples$run <- factor(rep(c("A","B"),each=3))
 samples[,c("sepal.width","sepal.length", "petal.width","petal.length","variety")]
 
+
+# So this won't fit with your data unless you make a directory called salmon, have quant.sf.gz files. I am not sure what the samples$species is doing but that
+# will not be needed. 
 files <- file.path(dir,"salmon", samples$species, "quant.sf.gz")
 names(files) <- samples$run
 tx2gene <- read_csv(file.path(dir, "tx2gene.gencode.v27.csv"))
@@ -28,6 +34,10 @@ dds$condition <- factor(dds$condition, levels = c("untreated","treated"))
 dds2 <- DESeq(dds)
 res <- results(dds2)
 res
+
+# you'll want ot pull the streing name from the levels and point to contrast table. This will feel oddd but writing out variable names as strings
+# won't automate. 
+
 res <- results(dds2, name="condition_treated_vs_untreated")
 res <- results(dds2, contrast=c("condition","treated","untreated"))
 resultsNames(dds2)
